ba142ed63b7375963a754139ea2472bf50596a90
Devastation <shivamwadatkar16@gmail.com>
2025-03-04 18:32:47 +0530

    first commit

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,14 +4,26 @@ import { AccountCard } from "./_components/account-card";
 import { CreateAccountDrawer } from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-
+import { getCurrentBudget } from "@/actions/budget";
+import BudgetProgress from "./_components/budget-progress";
 export default async function DashboardPage() {
   const [accounts] = await Promise.all([
     getUserAccounts(),
   ]);
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  let budgetData = null;
+
+  if(defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
   return (
-    <div className="space-y-8">
+    <div className="px-5">
+       {defaultAccount && <BudgetProgress
+       initialBudget = {budgetData?.budget}
+       currentExpenses = {budgetData?.currentExpenses || 0}/>}
+
       {/* Accounts Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
